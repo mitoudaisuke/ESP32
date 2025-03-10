5f6c668fcd3196abb34917533ae20a272c3b6700
mitoudaisuke <121172412+mitoudaisuke@users.noreply.github.com>
2025-03-10 14:34:31 +0900

    a

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ fig.subplots_adjust(hspace=0.5)  # 2つのグラフの間隔を広げる（値�
 ## **全体のトレンド**
 ax1.scatter(df["timestamp"], df["value"], color="black", alpha=0.3, s=1, label="raw data")
 ax1.plot(df["timestamp"], df["smoothed_value"], label="moving average", linewidth=1, color="orange")
-ax1.plot(df["timestamp"], df["smoothed_value"]+np.random.randint(-1000, 1001, size=len(df_recent)), label="moving average", linewidth=1, color="orange")
+ax1.plot(df["timestamp"], df["smoothed_value"]+np.random.randint(-100, 101, size=len(df_recent)), label="moving average", linewidth=1, color="orange")
 ax1.set_ylabel("gf", fontsize=14, fontweight="bold")
 ax1.set_title("LONG TREND", fontsize=14, fontweight="bold")
 ax1.grid(True, linestyle="--", linewidth=0.5, alpha=0.7)
