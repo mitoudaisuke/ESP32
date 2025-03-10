9ef241ad1d5156034c914588f08fdc0302ff5275
mitoudaisuke <121172412+mitoudaisuke@users.noreply.github.com>
2025-03-10 14:36:33 +0900

    a

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ def load_data():
 df = load_data()
 #st.title("WHOLE GARMENT©")
 st.markdown("### WHOLE GARMENT©")
+st.markdown("## MACHINE 01")
 # JST に変換（プロット時のみ）
 df["timestamp"] = df["timestamp"].dt.tz_localize('UTC').dt.tz_convert('Asia/Tokyo')
 
